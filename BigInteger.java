82fbb6ab6bb96d5e1b55bb0ee8bbfe6dcf342a7f
Elliot <e99pickens@gmail.com>
2018-05-15 22:07:05 -0500

    big ints

---
+++ BigInteger.java
@@ -0,0 +1,31 @@
+public class BigInteger {
+
+    private int[] bigInt;
+    private int sign;
+
+    public BigInteger(int[] newInt, int s) {
+        bigInt = newInt;
+        sign = s;
+    }
+
+    public int[] getInt() {
+        return bigInt;
+    }
+
+    public int getSign() {
+        return sign;
+    }
+
+    public int[] bigAddition(BigInteger intA) {
+
+    }
+
+    public int[] bigSubtraction(BigInteger intA) {
+
+    }
+
+    public  int[] bigMultiplication(BigInteger intA) {
+
+    }
+
+}
